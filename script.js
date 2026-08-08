31faa6f71cb03a772eccb0ed49afabaaccf8ff66
vnrlq33-del <vnrlq33@gmail.com>
2026-08-08 00:00:39 -0300

    script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const CONFIG = {
 
   // Sua foto. Coloque o arquivo em assets/ e escreva o caminho aqui.
   // Ex.: "assets/modelo.jpg"
-  foto: "assets/modelo.jpg",
+  foto: "assets/1.jpg",
 
   // Links das suas redes (troque pelos seus @)
   instagram: "https://instagram.com/_laviniacarraro",
